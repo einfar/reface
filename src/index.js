5c2de9b800155e9b634602defc9e34590b16e502
lisunovdv <lisunovdv@gmail.com>
2016-11-23 00:01:54 +0200

    Fix by @einfar comments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Timer from './Timer';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-function tick() {
-    ReactDOM.render(<Timer />, document.getElementById('timer'));
-}
-
-setInterval(tick, 1000);
